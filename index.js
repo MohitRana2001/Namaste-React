f06f4c63111b7c99fac573eb5edd110daf373e2d
MOHIT RANA <awesomemohit20@gmail.com>
2024-07-03 02:07:18 +0530

    imported react

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+/*
+*
+Parcel is continuously reloading which is also know as :- 
+Hot Module Replacement
+
+File Watcher Algorithm :- C++
+
+PARCEL :- 
+Bundling and Minification our code,
+Cleaning our code,
+Dev and Production Buil,
+Image Optimizatio,
+Caching while development,
+Compression,
+Compatible wiht older versions of browser,
+It adds polyfils,
+HTTPS on dev,
+Manages Port NO.,
+Consistent Hashing Algorithm,
+Zero Config Bundler,
+Transitive Dependencies :- To make a production ready
+    application we use serveral modules and they need several
+    dependencies. There is a dependency tree between these 
+    dependencies.
+*
+*
+*
+*/
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const heading1 = React.createElement("h1", {
     id : "heading2",
 }, "Namaste Everyone")
